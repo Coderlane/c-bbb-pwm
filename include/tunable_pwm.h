b1249403040fef09ca44ecc163cd27df240adb23
Travis Lane <travis.lane.osx@gmail.com>
2014-10-08 09:50:07 -0700

    working on basic layout of new structs

--- include/tunable_pwm.h
+++ include/tunable_pwm.h
@@ -6,9 +6,19 @@
  * @date 2014-10-05
  */
 
-
 #ifndef TUNABLE_PWM_H
 #define TUNABLE_PWM_H
+
+enum tpwm_duty_limit_e {
+	TPWM_DUTY_MAX_PERCENT = 100,
+	TPWM_DUTY_MIN_PERCENT = -100 
+};
+
+enum tpwmcs_duty_defaults_e {
+	TPWMCS_DUTY_DEFAULT_HIGH = 20000,
+	TPWMCS_DUTY_DEFAULT_OFF = 10000,
+	TPWMCS_DUTY_DEFAULT_LOW = 5000
+};
 
 struct tpwm_controller_t;
 struct tpwm_controller_settings_t;
--- include/tunable_pwm_internal.h
+++ include/tunable_pwm_internal.h
@@ -11,13 +11,48 @@
 #define TUNABLE_PWM_INTERNAL_H
 
 #include <tunable_pwm.h>
+#include <stdint.h>
 
-struct tpwm_controller_t {
-
-};
-
+/**
+ * @brief Settings for a tunable pwm controller.
+ */
 struct tpwm_controller_settings_t {
 
+	/**
+	 * @brief The high duty length.
+	 */
+	int16_t 	tpwmcs_duty_high;
+
+	/**
+	 * @brief The stop duty length.
+	 */
+	int16_t 	tpwmcs_duty_stop;
+
+	/**
+	 * @brief The low duty length.
+	 */
+	int16_t 	tpwmcs_duty_low;
+
+	/**
+	 * @brief The cycle length.
+	 */
+	uint16_t 	tpwmcs_cycle;
+};
+
+/**
+ * @brief A controller of a pwm source.
+ */
+struct tpwm_controller_t {
+	
+	/**
+	 * @brief Settings related to the controller.
+	 */
+	struct tpwm_controller_settings_t tpmpc_settings;
+
+	/**
+	 * @brief The scaled percentage of the duty cycle.
+	 */
+	int16_t tpwmc_duty_percent;
 };
 
 #endif /* TUNABLE_PWM_INTERNAL_H */
