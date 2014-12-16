1b8ed5f285ab8f1e31b636d9cfd449e0116ea550
Travis Lane <travis.lane.osx@gmail.com>
2014-12-16 12:12:16 -0800

    Add utilities to enable/disable pwm

--- util/disable_pwm.c
+++ util/disable_pwm.c
@@ -1,8 +1,20 @@
 
 
 #include <bbb_capemgr.h>
+#include <stdio.h>
 
 int main() {
+	int result = 0;
+	struct bbb_capemgr_t* bc;
 
-	return 0;
+	bc = bbb_capemgr_new();
+	result = bbb_capemgr_disable(bc, "am33xx_pwm");
+
+	if(result < 0) {
+		fprintf(stderr, "Error disableing pwm.\n");
+	}
+
+	bbb_capemgr_delete(&bc);
+
+	return result; 
 }
