be69d747c4f153080bf8e5a511645634d8fb36a0
Travis Lane <travis.lane.osx@gmail.com>
2014-12-15 23:07:09 -0800

    update test again

--- test/test_find_capemgr.c
+++ test/test_find_capemgr.c
@@ -11,11 +11,16 @@ int main() {
 	struct udev *probe_udev;
 
 	probe_udev = udev_new();
-	assert(probe_udev != NULL);
+	if(probe_udev == NULL) {
+		fprintf(stderr, "Failed to setup udev.\n");
+		return -1;
+	}
 
 	capemgr_path = find_device_syspath(probe_udev, "driver", "bone-capemgr");
-	assert(capemgr_path != NULL);
-
+	if(capemgr_path == NULL) {
+		fprintf(stderr, "Failed to find capemgr_path.\n");
+	}
+	
 	printf("capemgr path: %s\n", capemgr_path);
 	free(capemgr_path);
 	return 0;
