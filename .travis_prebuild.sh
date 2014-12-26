deccab77a811f7ba1b6fa596b47dcb292a4f7b99
Travis Lane <travis.lane.osx@gmail.com>
2014-12-26 14:35:46 -0800

    Fixed cmake files

--- .travis_prebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#wget http://www.coderlane.com/resources/cmake-3.1.tar.gz -4
-#wget http://www.coderlane.com/resources/libudev-dev.tar.gz -4
-tar -xf ./cmake-3.1.tar.gz
-tar -xf ./libudev-dev.tar.gz
