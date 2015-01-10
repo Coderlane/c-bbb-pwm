0b895f4cd51b60ed17d90aeef8cad88e0d89e7df
Travis Lane <travis.lane.osx@gmail.com>
2015-01-09 21:19:35 -0800

    Wrote code to search for a pwms

--- include/bbb_pwm.h
+++ include/bbb_pwm.h
@@ -11,35 +11,38 @@
 #define BBB_PWM_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
-enum bbb_pwm_state_e {
-  /**
-   * @brief The pwm is unclaimed.
-   */
-  BPS_UNCLAIMED = 0,
+enum bbb_pwm_state_e
+{
+    /**
+     * @brief The pwm is unclaimed.
+     */
+    BPS_UNCLAIMED = 0,
 
-  /**
-   * @brief The pwm is claimed.
-   */
-  BPS_CLAIMED = 1
+    /**
+     * @brief The pwm is claimed.
+     */
+    BPS_CLAIMED = 1
 };
 
-enum bbb_pwm_return_code_e {
-  BPRC_NOT_IMPLEMENTED = -100,
-  BPRC_BAD_WRITE = -59,
-  BPRC_NULL_PTR = -58,
-  BPRC_BAD_FILE = -57,
-  BPRC_RANGE = -56,
-  BPRC_NOT_CLAIMED = -55,
-  BPRC_BUSY = -54,
-  BPRC_PWM_NOT_FOUND = -53,
-  BPRC_DUPLICATE = -52,
-  BPRC_NO_PWM = -51,
-  BPRC_NO_CAPEMGR = -50,
-  BPRC_NO_DATA = -4,
-  BPRC_NO_MEM = -3,
-  BPRC_ERROR = -2,
-  BPRC_OK = 0
+enum bbb_pwm_return_code_e
+{
+    BPRC_NOT_IMPLEMENTED = -100,
+    BPRC_BAD_WRITE = -59,
+    BPRC_NULL_PTR = -58,
+    BPRC_BAD_FILE = -57,
+    BPRC_RANGE = -56,
+    BPRC_NOT_CLAIMED = -55,
+    BPRC_BUSY = -54,
+    BPRC_PWM_NOT_FOUND = -53,
+    BPRC_DUPLICATE = -52,
+    BPRC_NO_PWM = -51,
+    BPRC_NO_CAPEMGR = -50,
+    BPRC_NO_DATA = -4,
+    BPRC_NO_MEM = -3,
+    BPRC_ERROR = -2,
+    BPRC_OK = 0
 };
 
 struct bbb_pwm_t;
@@ -47,6 +50,10 @@ struct bbb_pwm_controller_t;
 
 struct bbb_pwm_controller_t *bbb_pwm_controller_new();
 void bbb_pwm_controller_delete(struct bbb_pwm_controller_t **bpc_ptr);
+bool bbb_pwm_controller_has_pwm(struct bbb_pwm_controller_t *bpc,
+                                const char *name);
+struct bbb_pwm_t* bbb_pwm_controller_get_pwm(struct bbb_pwm_controller_t *bpc,
+        const char *name);
 
 int bbb_pwm_claim(struct bbb_pwm_t *bp);
 int bbb_pwm_unclaim(struct bbb_pwm_t *bp);
--- include/bbb_pwm_internal.h
+++ include/bbb_pwm_internal.h
@@ -11,92 +11,100 @@
 #define BBB_PWM_INTERNAL_H
 
 #define _GNU_SOURCE
-
 #include <stdio.h>
 
 #include "bbb_pwm.h"
 
+struct udev;
+
 /**
  * @brief Represents a detected pwm.
  */
-struct bbb_pwm_t {
+struct bbb_pwm_t
+{
 
-  /**
-   * @brief The current state of the pwm.
-   */
-  enum bbb_pwm_state_e    bp_state;
+	/**
+	 * @brief The current state of the pwm.
+	 */
+	enum bbb_pwm_state_e    bp_state;
 
-  /**
-   * @brief The name of this pwm.
-   */
-  char 									 *bp_name;
+	/**
+	 * @brief The name of this pwm.
+	 */
+	char                                   *bp_name;
 
-  /**
-   * @brief The cached pwm duty cycle.
-   */
-  uint32_t 								bp_duty_cycle;
+	/**
+	 * @brief The cached pwm duty cycle.
+	 */
+	uint32_t                              bp_duty_cycle;
 
-  /**
-   * @brief The cached pwm period (in nanoseconds).
-   */
-  uint32_t 								bp_period;
+	/**
+	 * @brief The cached pwm period (in nanoseconds).
+	 */
+	uint32_t                              bp_period;
 
-  /**
-   * @brief The cached pwm polarity.
-   */
-  int8_t 									bp_polarity;
+	/**
+	 * @brief The cached pwm polarity.
+	 */
+	int8_t                                    bp_polarity;
 
-  /**
-   * @brief The file path of the duty file.
-   */
-  char 									 *bp_duty_file_path;
+	/**
+	 * @brief The file path of the duty file.
+	 */
+	char                                   *bp_duty_file_path;
 
-  /**
-   * @brief The file path of the period file.
-   */
-  char 									 *bp_period_file_path;
+	/**
+	 * @brief The file path of the period file.
+	 */
+	char                                   *bp_period_file_path;
 
-  /**
-   * @brief The file path of the polarity file.
-   */
-  char 									 *bp_polarity_file_path;
+	/**
+	 * @brief The file path of the polarity file.
+	 */
+	char                                   *bp_polarity_file_path;
 
-  /**
-   * @brief The file with the pwm's duty data.
-   */
-  FILE 									 *bp_duty_file;
+	/**
+	 * @brief The file with the pwm's duty data.
+	 */
+	FILE                                   *bp_duty_file;
 
-  /**
-   * @brief The file with the pwm's period data.
-   */
-  FILE 									 *bp_period_file;
+	/**
+	 * @brief The file with the pwm's period data.
+	 */
+	FILE                                   *bp_period_file;
 
-  /**
-   * @brief The file with the pwm's polarity data.
-   */
-  FILE 									 *bp_polarity_file;
+	/**
+	 * @brief The file with the pwm's polarity data.
+	 */
+	FILE                                   *bp_polarity_file;
 
-  /**
-   * @brief The next PWM in the list of PWMs, NULL signifies the end.
-   */
-  struct bbb_pwm_t		 	 *bp_next;
+	/**
+	 * @brief The next PWM in the list of PWMs, NULL signifies the end.
+	 */
+	struct bbb_pwm_t           *bp_next;
 };
 
 /**
  * @brief A controller enables/disables pwm functionality.
  * It also detects pwms and makes them avaliable for use.
  */
-struct bbb_pwm_controller_t {
+struct bbb_pwm_controller_t
+{
 
-  /**
-   * @brief The number of pwms detected.
-   */
-  int8_t 									bpc_num_pwms;
+	/**
+	 * @brief The number of pwms detected.
+	 */
+	int8_t                                    bpc_num_pwms;
 
-  /**
-   * @brief The individual pwms.
-   */
-  struct bbb_pwm_t 	 		 *bpc_head_pwm;
+	/**
+	 * @brief The individual pwms.
+	 */
+	struct bbb_pwm_t           *bpc_head_pwm;
+
+	/**
+	 * @brief
+	 */
+	struct udev                      *bpc_udev;
 };
 
 int bbb_pwm_controller_init(struct bbb_pwm_controller_t *bpc);
@@ -104,6 +112,7 @@ int bbb_pwm_controller_add_pwm(struct bbb_pwm_controller_t *bpc,
                                struct bbb_pwm_t *bp);
 int bbb_pwm_controller_remove_pwm(struct bbb_pwm_controller_t *bpc,
                                   const char *name);
+
 int bbb_pwm_controller_probe(struct bbb_pwm_controller_t *bpc);
 
 struct bbb_pwm_t *bbb_pwm_new(const char *name, const char *root_path);
